3a6a010739ca050f356c92c898315b9e77bf40b6
jacksonn455 <46221221+jacksonn455@users.noreply.github.com>
2019-06-14 15:36:51 -0300

    Add files via upload

---
+++ modificadores.dart
@@ -0,0 +1,20 @@
+import 'getterEsetter.dart';
+
+class Valores{
+
+static int vezesClicado;
+
+}
+
+class Pessoa{
+
+}
+
+void main(){
+
+Valores.vezesClicado = 2;
+const numero = 3;
+
+final Pessoa pessoa = Pessoa();
+
+}
